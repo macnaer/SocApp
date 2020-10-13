7076d66a43875ebce998a4bea1914184ac0aa606
k2 <trofimchuk.an@gmail.com>
2020-10-14 00:12:34 +0300

    Activity list ans details

--- API/Program.cs
+++ API/Program.cs
@@ -24,6 +24,7 @@ namespace API
                 {
                     var context = services.GetRequiredService<DataContext>();
                     context.Database.Migrate();
+                    seed.SeedData(context);
                 }
                 catch (Exception ex)
                 {
