92515b2d24dec6969cfa18bf03aff8749531d17e
k2 <trofimchuk.an@gmail.com>
2020-10-17 21:50:33 +0300

    Finish UI skeleton

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,11 +1,45 @@
-import React from "react";
+import React, { useState, useEffect, Fragment } from "react";
+import axios from "axios";
+import { Container } from "semantic-ui-react";
+import { IActivity } from "../models/activity";
+import { Header, Icon, List } from "semantic-ui-react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">Learn React</header>
-    </div>
-  );
+interface IState {
+  activities: IActivity[];
 }
 
+const App = () => {
+  const [activities, setActivities] = useState<IActivity[]>([]);
+  const [selectedActivity, setSelectedActivity] = useState<IActivity | null>(
+    null
+  );
+  const [editMode, setEditMode] = useState(false);
+
+  useEffect(() => {
+    axios
+      .get<IActivity[]>("http://localhost:5000/api/activities")
+      .then((response) => {
+        let activities: IActivity[] = [];
+        response.data.forEach((activity) => {
+          activity.date = activity.date.split(".")[0];
+          activities.push(activity);
+        });
+        setActivities(activities);
+      });
+  }, []);
+
+  return (
+    <div>
+      <Header as="h2">
+        <Icon name="users" />
+        <Header.Content>Reactivities</Header.Content>
+      </Header>
+      <List>
+        {activities.map((activity) => (
+          <List.Item key={activity.id}>{activity.title}</List.Item>
+        ))}
+      </List>
+    </div>
+  );
+};
 export default App;
